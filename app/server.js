34f3d560b4ca69ab7d3dc59d733ba315707a437c
Gkemhcs <gudikotieswarmani@gmail.com>
2023-12-06 11:55:36 +0000

    code added

--- app/server.js
+++ app/server.js
@@ -4,6 +4,7 @@ app.set("view engine","ejs")
 app.get("/",(req,res)=>{
     res.send("welcome to gkemhcs world")
 })
+s="iidfi2f2i2j9i9i9i9kok129k0ei20id20e10ej0ej120e10ek21oek1ek01"
 app.listen(8080,()=>{
     console.log("server started")
 })
