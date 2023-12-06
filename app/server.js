ed417a3707b0358126f93e3ca910e7b531b535e7
Gkemhcs <gudikotieswarmani@gmail.com>
2023-12-06 11:45:29 +0000

    code added

---
+++ app/server.js
@@ -0,0 +1,9 @@
+const express=require("express")
+const app=express()
+app.set("view engine","ejs")
+app.get("/",(req,res)=>{
+    res.send("welcome to gkemhcs world")
+})
+app.listen(8080,()=>{
+    console.log("server started")
+})
